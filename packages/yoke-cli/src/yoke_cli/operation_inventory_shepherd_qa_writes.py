9aaf55c87dba4c64f5fce0d76c5c7bc9f69200ef
Ben Bauman <beebauman@gmail.com>
2026-09-20 02:25:59 -0400

    Register the declare-none adapter in the CLI and operation inventories

--- packages/yoke-cli/src/yoke_cli/operation_inventory_shepherd_qa_writes.py
+++ packages/yoke-cli/src/yoke_cli/operation_inventory_shepherd_qa_writes.py
@@ -10,6 +10,7 @@ from yoke_cli.operation_inventory_model import _Row, _w
 WRAPPED_ROWS: Tuple[_Row, ...] = (
     _w("yoke qa requirement waive", "qa.requirement"),
     _w("yoke qa requirement supersede", "qa.requirement"),
+    _w("yoke qa post-deploy declare-none", "qa.post_deploy"),
     _w("yoke items dependency add", "items.dependency"),
     _w("yoke items dependency update", "items.dependency"),
     _w("yoke items dependency remove", "items.dependency"),
